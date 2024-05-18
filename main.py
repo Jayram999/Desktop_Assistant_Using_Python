5bc1281ba4f9d25942f4dfdd6d419b387a46a882
Jayram999 <jayramnandagiri@gmail.com>
2024-05-18 12:06:29 +0530

    update main

--- main.py
+++ main.py
@@ -12,32 +12,72 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 engine.setProperty('rate', 150)
 
-def speak(audio):
-    '''This function takes text and returns voice
-    Args: text
-    '''
-    engine.say(audio)
+
+#speak function
+def speak(text):
+    """This function takes text and returns voice
+
+    Args:
+        text (_type_): string
+    """
+    engine.say(text)
     engine.runAndWait()
 
-speak('Hello Jayram, How are you?')    
 
+# speech recognition function
 def takeCommand():
-    '''This function takes speech and returns text
-    Args: audio
-    '''
+    """this function will recognize voice & return text
+    """
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print('listening....')
+        print("Listening...")
         r.pause_threshold = 1
-        audio = r.listen(source, timeout=3, phrase_time_limit=5)
-        
-    try:
-        print("Recognizing....")
-        query = r.recognize_google(audio, language= 'en-in')
-        print("Let's talk about {}.".format(query))
-    except Exception as e:
-        print("voice not recognized")  
+        audio = r.listen(source,timeout  = 5,phrase_time_limit =5)
+        '''
+        Explanation
+        The pause_threshold value is the number of seconds the system will take to recognize the voice after the user has completed their sentence.
+        The timeout value is the maximum number of seconds the system will wait for the user to say something before it throws an OSError exception.
+        The phrase_time_limit value indicates the number of seconds the user can speak. In this case, it is 5. 
+        This means that if the user will speak for more than 5 seconds, that speech will not be recognized.
+        '''
 
 
-takeCommand()
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}\n")
 
+        except Exception as e:
+            print("Say that again please...")
+            return "None"
+        return query
+
+
+
+
+if __name__ == "__main__":
+    
+    query = takeCommand().lower()
+
+    if "wikipedia" in query:
+        speak("Searching wikipedia")
+        query = query.replace('wikipedia', "")
+        results = wikipedia.summary(query, sentences = 2)
+        speak("According to wikipedia")
+        print(results)
+        speak(results)
+
+    
+    elif "youtube" in query:
+        speak("Opening YouTube")
+        webbrowser.open("youtube.com")
+
+    
+    elif "google" in query:
+        speak("Opening google")
+        webbrowser.open("google.com")
+
+
+    elif "github" in query:
+        speak("Opening github")
+        webbrowser.open("github.com")
